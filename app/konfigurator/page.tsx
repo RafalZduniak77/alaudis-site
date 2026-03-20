5cd66fb3c4da1d8a35db0b10919fb2c126af795f
WICHURA <R.Z@SAPRENOVATION.COM>
2026-03-20 09:40:18 +0100

    fix konfigurator routing

---
+++ app/konfigurator/page.tsx
@@ -0,0 +1,13 @@
+//
+//  page.tsx
+//  
+//
+//  Created by Rafal Zduniak on 20/03/2026.
+//
+"use client";
+
+import KonfiguratorShell from "@/components/konfigurator/KonfiguratorShell";
+
+export default function KonfiguratorPage() {
+  return <KonfiguratorShell />;
+}
