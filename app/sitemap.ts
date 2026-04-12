1250893655da1df8b18b2445ad40da92c275b00c
WICHURA <R.Z@SAPRENOVATION.COM>
2026-04-12 10:11:28 +0200

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+// ==========================================================
+// SITEMAP - ALAUDIS
+// ==========================================================
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://alaudis.eu",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://alaudis.eu/odkryj-modele",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://alaudis.eu/konfigurator",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://alaudis.eu/kontakt",
+      lastModified: new Date(),
+    },
+  ];
+}
