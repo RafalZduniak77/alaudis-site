70ec02a704721bddb04603ae78798b82eca0b853
WICHURA <R.Z@SAPRENOVATION.COM>
2026-05-02 09:52:04 +0200

    Wrap configurator pages in Suspense

--- app/en/konfigurator/page.tsx
+++ app/en/konfigurator/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import KonfiguratorShell from "../../../components/konfigurator/KonfiguratorShell";
 
 export default function KonfiguratorPage() {
-  return <KonfiguratorShell />;
+  return (
+    <Suspense fallback={<main className="min-h-screen bg-black" />}>
+      <KonfiguratorShell />
+    </Suspense>
+  );
 }
